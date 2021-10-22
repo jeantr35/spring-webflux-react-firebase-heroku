5173129e1f386294e55d41f2e78e84094751de6b
jeantr35 <jean.trujillo.35@gmail.com>
2021-10-22 18:16:47 -0500

    added the test to createQuestion & answer usecase

--- src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
@@ -1,7 +1,57 @@
 package co.com.sofka.questions.usecases;
 
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AddAnswerUseCaseTest {
 
+    @SpyBean
+    AddAnswerUseCase addAnswerUseCase;
+
+    @MockBean
+    GetUseCase getUseCase;
+
+    @MockBean
+    AnswerRepository answerRepository;
+
+    @Test
+    @DisplayName("Add answer")
+    void answerTest(){
+
+        var questionDTO = new QuestionDTO("XXXXX", "userid", "test", "open", "test", "aaaa@gmail.com", "aaa");
+
+        var answerDTO = new AnswerDTO("XXXXX","aaa","XXXXX","test", "aaaaa", "aaa");
+        var answer = new Answer();
+        answer.setId("aaaaa");
+        answer.setQuestionId("XXXXX");
+        answer.setUserId("aaa");
+        answer.setAnswer("XXXXX");
+        answer.setEmail("test");
+        answer.setUserPhotoUl("aaa");
+
+        Mockito.when(answerRepository.save(Mockito.any(Answer.class))).thenReturn(Mono.just(answer));
+        Mockito.when(getUseCase.apply(Mockito.anyString())).thenReturn(Mono.just(questionDTO));
+
+        var resultDTO = addAnswerUseCase.apply(answerDTO);
+        var resultQuestionDTO = resultDTO.block();
+
+        assert resultQuestionDTO != null;
+        Assertions.assertEquals(resultQuestionDTO.getId(),questionDTO.getId());
+        Assertions.assertEquals(resultQuestionDTO.getQuestion(),questionDTO.getQuestion());
+        Assertions.assertEquals(resultQuestionDTO.getAnswers().get(0).getQuestionId(),answerDTO.getQuestionId());
+
+    }
 }
--- src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
@@ -1,7 +1,51 @@
 package co.com.sofka.questions.usecases;
 
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CreateUseCaseTest {
 
+    @SpyBean
+    CreateUseCase createUseCase;
+
+    @MockBean
+    QuestionRepository questionRepository;
+
+
+    @Test
+    @DisplayName("Create question")
+    void createQuestionTest() {
+
+        var questionDTO = new QuestionDTO("XXXXX", "userid", "test", "open", "test", "aaaa@gmail.com", "aaa");
+        var question = new Question();
+        question.setId("XXXXX");
+
+        Mockito.when(questionRepository.save(Mockito.any(Question.class))).thenReturn(Mono.just(question));
+
+        StepVerifier.create(createUseCase.apply(questionDTO)).expectNextMatches(
+                id -> {
+                    assert id.equals(questionDTO.getId());
+                    return true;
+                }).verifyComplete();
+
+    }
 }
