65e0cbb14868be1bc533eb2aa56d1ac2002ff04a
jeantr35 <jean.trujillo.35@gmail.com>
2021-10-22 18:29:02 -0500

    added the delete test

--- src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
@@ -1,7 +1,30 @@
 package co.com.sofka.questions.usecases;
 
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class DeleteUseCaseTest {
+    @SpyBean
+    private DeleteAnswerUseCase deleteAnswerUseCase;
 
+    @MockBean
+    private AnswerRepository answerRepository;
+
+    @Test
+    @DisplayName("delete question")
+    void deleteAnswerTest(){
+        Mockito.when( answerRepository.deleteById("XXXXXX")).thenReturn(Mono.empty());
+
+        var result = deleteAnswerUseCase.apply("XXXXXX").block();
+        assertNull(result);
+    }
 }
